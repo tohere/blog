2ea24dd4b2cb6bb469b7f6e1bd3fee479aa86839
tomorrow-here <2878662513@qq.com>
2019-01-15 22:51:18 +0800

    后台部分布局完成

---
+++ src/views/admin/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 管理员页面 -->
+  <div class="editor">
+    <mavon-editor></mavon-editor>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.editor {
+  width: 100%;
+  height: 100%;
+}
+.v-note-wrapper {
+  height: 100%;
+}
+</style>
